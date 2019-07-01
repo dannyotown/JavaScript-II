09bbeefb74386bc8c0b94261f4684deaa5f64b03
Danny Town <47186976+dannyotown@users.noreply.github.com>
2019-07-01 11:16:06 -0500

    Not sure about counter factory

--- assignments/closure.js
+++ assignments/closure.js
@@ -7,11 +7,12 @@ function GrandPa(){
     function Mom(){
       console.log('School is for fools');
     }
-    mom();
+    Mom();
   }
-  dad();
+  Dad();
 }
 GrandPa();
+Mom();
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
@@ -19,14 +20,34 @@ GrandPa();
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let count = 0;
+  return function(){
+    count +=1;
+    return count;
+  }
 };
-// Example usage: const newCounter = counter();
-// newCounter(); // 1
-// newCounter(); // 2
+const newCounter = counter();
+console.log(newCounter());
+console.log(newCounter());
+
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+  let count = 0;
+  return newObject = [
+    increment = function(){
+      count += 1
+      return count;
+    },
+    decrement = function(){
+      count -=1;
+      return count;
+    }
+  ]
 };
+
+const newFactory = counterFactory();
+console.log(newFactory);
